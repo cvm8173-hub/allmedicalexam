91b80e83c9e6d964102b980ff3c88421dd922505
cvm8173-hub <cvm8173@gmail.com>
2026-02-28 11:28:03 +0530

    Change counter initialization and page range for scraping

--- allexam.py
+++ allexam.py
@@ -2191,12 +2191,12 @@ def extract_cat_MArch_data(driver, URLS):
 
 if __name__ == "__main__":
     driver = create_driver()
-    counter = 1
+    counter = 31
 
     try:
         final_data = []
 
-        for page in range(1, 5):
+        for page in range(5, 9):
             print(f"Scraping listing page {page}")
 
             exams = scrape_listing_page(driver, page)
